b93be04a5c08dbf8fe1d3d1645da6717e5f52002
John Tomlinson <jtomlinson@brightree.com>
2021-11-16 15:09:21 +0000

    2nd commit
    * Fixed Index view
    * Created Details view
    * Started to create Edit view

--- AnnualLeaveRequestToolMVC/Controllers/AnnualLeaveRequestController.cs
+++ AnnualLeaveRequestToolMVC/Controllers/AnnualLeaveRequestController.cs
@@ -2,15 +2,17 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AnnualLeaveRequestToolMVC.Controllers
 {
     public class AnnualLeaveRequestController : Controller
     {
-        private static List<AnnualLeaveRequestOverviewModel> _anuualLeaveRequests = new List<AnnualLeaveRequestOverviewModel>()
+        private static List<AnnualLeaveRequestOverviewModel> _annualLeaveRequests = new List<AnnualLeaveRequestOverviewModel>()
         {
             new AnnualLeaveRequestOverviewModel()
             {
+                AnnualLeaveRequestID = 1,
                 Year = 2021,
                 StartDate = new DateTime(2021,01,01),
                 ReturnDate = new DateTime(2021,01,06),
@@ -23,9 +25,11 @@ namespace AnnualLeaveRequestToolMVC.Controllers
                 NumberOfDaysLeft = 24,
                 NumberOfAnnualLeaveDaysLeft = 22,
                 NumberOfPublicLeaveDaysLeft = 2,
+                Notes = "Xmas and New Year",
             },
             new AnnualLeaveRequestOverviewModel()
             {
+                AnnualLeaveRequestID = 2,
                 Year = 2021,
                 StartDate = new DateTime(2021,07,05),
                 ReturnDate = new DateTime(2021,07,12),
@@ -38,9 +42,11 @@ namespace AnnualLeaveRequestToolMVC.Controllers
                 NumberOfDaysLeft = 19,
                 NumberOfAnnualLeaveDaysLeft = 17,
                 NumberOfPublicLeaveDaysLeft = 2,
+                Notes = "Summer Holiday",
             },
              new AnnualLeaveRequestOverviewModel()
             {
+                AnnualLeaveRequestID = 3,
                 Year = 2021,
                 StartDate = new DateTime(2021,12,20),
                 ReturnDate = new DateTime(2021,12,31),
@@ -53,12 +59,47 @@ namespace AnnualLeaveRequestToolMVC.Controllers
                 NumberOfDaysLeft = 10,
                 NumberOfAnnualLeaveDaysLeft = 10,
                 NumberOfPublicLeaveDaysLeft = 0,
+                Notes = "Xmas and New Year",
             },
         };
 
+        private AnnualLeaveRequestOverviewModel GetAnnualLeaveRequest(int annualLeaveRequestID) => _annualLeaveRequests
+                                                                                        .Where(x => x.AnnualLeaveRequestID == annualLeaveRequestID)
+                                                                                        .FirstOrDefault();
+
         public IActionResult Index()
         {
-            return View(_anuualLeaveRequests);
+            return View(_annualLeaveRequests);
+        }
+
+        public IActionResult Details(int annualLeaveRequestId)
+        {
+            var annualLeaveRequest = GetAnnualLeaveRequest(annualLeaveRequestId);
+
+            return View(annualLeaveRequest);
+        }
+
+        public IActionResult Edit(int annualLeaveRequestId)
+        {
+            var annualLeaveRequest = GetAnnualLeaveRequest(annualLeaveRequestId);
+
+            return View(annualLeaveRequest);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AnnualLeaveRequestOverviewModel editAnnualLeaveRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                int annualLeaveRequestIndex = _annualLeaveRequests.FindIndex(x => x.AnnualLeaveRequestID == editAnnualLeaveRequest.AnnualLeaveRequestID);
+                _annualLeaveRequests[annualLeaveRequestIndex] = editAnnualLeaveRequest;
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(editAnnualLeaveRequest);
+            }
         }
     }
 }
