685ad16c37c5e8acf33ccb62d3d6914fa9ddf990
John Tomlinson <jtomlinson@brightree.com>
2021-11-24 08:35:53 +0000

    Created new Annual Leave database project to be able to generate the database schema and scripts

---
+++ AnnualLeaveDatabase/dbo/Views/AnnualLeaveRequestsOverview.sql
@@ -0,0 +1,26 @@
+﻿
+CREATE VIEW [dbo].[AnnualLeaveRequestsOverview]
+AS
+
+    select 
+		ar.AnnualLeaveRequestID as AnnualLeaveRequestID
+		,ay.Year as Year
+		,ar.PaidLeaveType as PaidLeaveType
+		,ar.LeaveType as LeaveType
+		,ar.StartDate as StartDate
+		,ar.ReturnDate as ReturnDate
+		,ar.NumberOfDays as NumberOfDaysRequested
+		,ar.NumberOfAnnualLeaveDays as NumberOfAnnualLeaveDaysRequested
+		,ar.NumberOfPublicLeaveDays as NumberOfPublicLeaveDaysRequested
+		,ar.DateCreated as DateCreated
+		,ar.Notes as Notes
+		,ay.NumberOfDays as NumberOfDays
+		,ay.NumberOfAnnualLeaveDays as NumberOfAnnualLeaveDays
+		,ay.NumberOfPublicLeaveDays as NumberOfPublicLeaveDays
+		,ay.NumberOfDaysLeft as NumberOfDaysLeftForYear
+		,ay.NumberOfAnnualLeaveDaysLeft as NumberOfAnnualLeaveDaysLeftForYear
+		,ay.NumberOfPublicLeaveDaysLeft as NumberOfPublicLeaveDaysLeftForYear
+    from 
+		AnnualLeaveYear ay 
+    left join AnnualLeaveRequest ar on ar.Year = ay.Year
+
