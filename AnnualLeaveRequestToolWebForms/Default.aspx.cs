65f4e1d0d5289c6374d7458713df65a030639c20
John Tomlinson <33494306+johnt84@users.noreply.github.com>
2022-02-17 16:10:17 +0000

    Update Default.aspx.cs

--- AnnualLeaveRequestToolWebForms/Default.aspx.cs
+++ AnnualLeaveRequestToolWebForms/Default.aspx.cs
@@ -9,7 +9,7 @@ namespace AnnualLeaveRequestToolWebForms
 {
     public partial class _Default : Page
     {
-        protected void Page_Load(object sender, EventArgs e) 
+        protected void Page_Load(object sender, EventArgs e)
         {
 
         }
