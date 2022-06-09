a1a7c4f763477460c387d77926d88d4a921f9501
John Tomlinson <jtomlinson@brightree.com>
2022-06-09 15:01:30 +0100

    Initial commit to create a Blazor Maui version of the Annual Leave Request tool

---
+++ AnnualLeaveRequestToolBlazorMaui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AnnualLeaveRequestToolBlazorMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AnnualLeaveRequestToolBlazorMaui/MauiProgram.cs
@@ -0,0 +1,43 @@
+﻿using AnnualLeaveRequest.Shared;
+using AnnualLeaveRequestDapperDAL;
+using AnnualLeaveRequestToolBlazorServerDapper.Data;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace AnnualLeaveRequestToolBlazorMaui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            var a = Assembly.GetExecutingAssembly();
+            using var stream = a.GetManifestResourceStream("AnnualLeaveRequestToolBlazorMaui.appsettings.json");
+
+            var config = new ConfigurationBuilder()
+              .AddJsonStream(stream)
+              .Build();
+
+            builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+
+            var sqlConnectionConfiguration = new SqlConnectionConfiguration(config.GetConnectionString("AnnualLeaveRequestDB"));
+            builder.Services.AddSingleton(sqlConnectionConfiguration);
+
+            builder.Services.AddSingleton<AnnualLeaveRequestDataAccess>();
+
+            builder.Services.AddSingleton<IAnnualLeaveRequestLogic, AnnualLeaveRequestLogic>();
+
+            return builder.Build();
+        }
+    }
+}
